70470b0d1744948241fe223f4d50cddab4a0279a
Nathan <nathan.dsouza316@gmail.com>
2021-09-15 12:14:50 +1200

    Remove Local Address from API

--- api.py
+++ api.py
@@ -3,13 +3,10 @@ import json
 
 def getResults(searchTerm:str, resultsAmount:int):
     try:
-        url = f'http://192.168.1.19:8090/yacysearch.json?query={searchTerm}&resource=global&urlmaskfilter=.*&prefermaskfilter=all&maximumRecords={resultsAmount}'
+        url = f'http://127.0.0.1:8090/yacysearch.json?query={searchTerm}&resource=global&urlmaskfilter=.*&prefermaskfilter=all&maximumRecords={resultsAmount}'
         response = urlopen(url)
         finalData = json.loads(response.read())
-        # print(finalData["channels"][0]["items"][0]["link"])
         return finalData["channels"][0]["items"]
     except Exception as e:
         print(e)    
         return {"error":"No result was found"}
-
-# getResults("minecraft", 100)
